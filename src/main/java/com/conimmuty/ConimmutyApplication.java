ab0a157f91c05b84ab818d3eaab4d9ccbf5eb413
riroan <riroan@naver.com>
2023-02-20 00:40:42 +0900

    Initial commit

---
+++ src/main/java/com/conimmuty/ConimmutyApplication.java
@@ -0,0 +1,13 @@
+package com.conimmuty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConimmutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConimmutyApplication.class, args);
+	}
+
+}
