1ac64cb3cd063503d8ec620520ead8615b135e1c
riroan <riroan@naver.com>
2023-02-20 04:39:31 +0900

    fix: post 안되는거 수정

--- src/main/java/com/conimmuty/controller/CommentController.java
+++ src/main/java/com/conimmuty/controller/CommentController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,7 +30,7 @@ public class CommentController {
     }
 
     @PostMapping("/comment")
-    public ResponseEntity<Void> save(CommentRequestDTO req) {
+    public ResponseEntity<Void> save(@RequestBody CommentRequestDTO req) {
         Long pid = req.getPid();
         String body = req.getBody();
         commentService.save(pid, body);
